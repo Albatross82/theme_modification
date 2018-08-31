772ed223b27786d42992ae557b0e13c3d5fe7f60
Albatross82 <shirocov@gmail.com>
2018-08-31 13:00:34 +0300

    Add files via upload

---
+++ theme-modifications.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Plugin Name: Theme Monstroid2 Integration
+ * Version: 0.0.9
+ * Date: 30.03.2018
+ * Author: Roy Shirocov
+ * Author URI: mailto:shirocov@gmail.com
+ * TODO: Replace all string literals with constants, add comments, provide README/quick start guide.
+**/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+
+require_once('includes/theme_integration.php');
+?>
